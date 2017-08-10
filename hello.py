e37cfcb6c723b37384b613f7695ea6566b5ca009
dsrakkar <dsrakkar@gmail.com>
2017-08-09 21:03:17 -0400

    sample files

---
+++ hello.py
@@ -0,0 +1,12 @@
+# This program says hello and asks for my name
+
+print('Hello World!')
+print('what is your name') # ask for you name
+myName = input()
+print('it is good to meet you, ' + myName)
+print('the length of your name:')
+print(len(myName))
+print('what is your age?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+      
---
+++ value input.py
@@ -0,0 +1,6 @@
+print('enter a name.')
+name = inputs()
+if name != '':
+    print('thank you for entering a name.')
+else:
+    print('you did not enter a name.')
